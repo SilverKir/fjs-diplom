091c484cc53521d61d26e636448fa7d483619d4b
silver_kir <rci@mp03.ru>
2025-08-14 19:58:20 +0800

    users list

--- src/navigate/navigate.constants.ts
+++ src/navigate/navigate.constants.ts
@@ -14,12 +14,11 @@ export const CLIENT_NAV: INavigation[] = [
 
 export const MANAGER_NAV: INavigation[] = [
   { id: 1, name: 'Поиск номеров', link: '/hotel-rooms' },
-  { id: 2, name: 'Список броней', link: '/manager/reservations' },
-  { id: 3, name: 'Cписок пользователей', link: '/manager/users/' },
-  { id: 4, name: 'Обращения в поддержку', link: '/manager/support-requests' },
+  { id: 2, name: 'Cписок пользователей', link: '/manager/users' },
+  { id: 3, name: 'Обращения в поддержку', link: '/manager/support-requests' },
 ];
 
 export const ADMIN_NAV: INavigation[] = [
-  { id: 1, name: 'Гостиницы', link: '/admin/hotels/' },
-  { id: 2, name: 'Cписок пользователей', link: '/admin/users/' },
+  { id: 1, name: 'Гостиницы', link: '/admin/hotels' },
+  { id: 2, name: 'Cписок пользователей', link: '/admin/users' },
 ];
